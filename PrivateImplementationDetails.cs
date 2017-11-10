0d73869daba2a37590a87b6727ef61d5e914a054
wana_chengwang <chengwang2000>
2017-11-11 02:10:10 +0800

    add PrivateImplementationDetails.cs

---
+++ PrivateImplementationDetails.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+internal class PrivateImplementationDetails
+{
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 68)]
+	private struct struct0x600015c_1
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 76)]
+	private struct struct0x600015c_2
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 68)]
+	private struct struct0x6000162_1
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 6144)]
+	private struct struct0x6000170_1
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 384)]
+	private struct struct0x6000170_2
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 124)]
+	private struct struct0x6000170_3
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 124)]
+	private struct struct0x6000170_4
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 120)]
+	private struct struct0x6000170_5
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 120)]
+	private struct struct0x6000170_6
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 1152)]
+	private struct struct0x6000173_1
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 120)]
+	private struct struct0x6000173_2
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 116)]
+	private struct struct0x600018a_1
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 120)]
+	private struct struct0x600018a_2
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 76)]
+	private struct struct0x600018a_3
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 19)]
+	private struct struct0x600018a_4
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 512)]
+	private struct struct0x600018a_5
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 256)]
+	private struct struct0x600018a_6
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 116)]
+	private struct struct0x600018a_7
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 120)]
+	private struct struct0x600018a_8
+	{
+	}
+
+	internal static Hashtable method0x600000d_1;
+    /*
+	internal static PrivateImplementationDetails.struct0x600015c_1 method0x600015c_1;
+
+	internal static PrivateImplementationDetails.struct0x600015c_2 method0x600015c_2;
+
+	internal static PrivateImplementationDetails.struct0x6000162_1 method0x6000162_1;
+
+	internal static PrivateImplementationDetails.struct0x6000170_1 method0x6000170_1;
+
+	internal static PrivateImplementationDetails.struct0x6000170_2 method0x6000170_2;
+
+	internal static PrivateImplementationDetails.struct0x6000170_3 method0x6000170_3;
+
+	internal static PrivateImplementationDetails.struct0x6000170_4 method0x6000170_4;
+
+	internal static PrivateImplementationDetails.struct0x6000170_5 method0x6000170_5;
+
+	internal static PrivateImplementationDetails.struct0x6000170_6 method0x6000170_6;
+
+	internal static PrivateImplementationDetails.struct0x6000173_1 method0x6000173_1;
+
+	internal static PrivateImplementationDetails.struct0x6000173_2 method0x6000173_2;
+
+	internal static PrivateImplementationDetails.struct0x600018a_1 method0x600018a_1;
+
+	internal static PrivateImplementationDetails.struct0x600018a_2 method0x600018a_2;
+
+	internal static PrivateImplementationDetails.struct0x600018a_3 method0x600018a_3;
+
+	internal static PrivateImplementationDetails.struct0x600018a_4 method0x600018a_4;
+
+	internal static PrivateImplementationDetails.struct0x600018a_5 method0x600018a_5;
+
+	internal static PrivateImplementationDetails.struct0x600018a_6 method0x600018a_6;
+
+	internal static PrivateImplementationDetails.struct0x600018a_7 method0x600018a_7;
+
+	internal static PrivateImplementationDetails.struct0x600018a_8 method0x600018a_8;
+    */
+	internal static Hashtable method0x600027e_1;
+
+	internal static Hashtable method0x600043e_1;
+}
